68183c83e20a933c095e74525f269b00a46a62c0
RaizinRaizen <raicampos3246@gmail.com>
2024-09-18 11:57:57 -0300

    Aula_06

---
+++ Aula06a.py
@@ -0,0 +1,13 @@
+n1 = int(input('digite um numero: '))
+n2 = int(input('digite outro numero: '))
+soma = n1 + n2
+print('A soma é {}'.format(soma))
+
+#Pratica da aula 006
+
+n1 = int(input('digite um numero: '))
+n2 = int(input('digite outro numero: '))
+s = n1 + n2
+print('A soma entre {} e {} é: {}'.format(n1, n2, s))
+print(type(s))
+ 
